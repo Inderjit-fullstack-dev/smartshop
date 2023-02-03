f6c678a2e41d8d6135b0d7c8f59c053576d0a46a
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-02-03 13:29:45 +0530

    redis client configuration

---
+++ Microservices/Smartshop.Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace Smartshop.Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+        public ShoppingCart(string username)
+        {
+            Username = username;
+            Items = new List<ShoppingCartItem>();
+        }
+
+        public decimal TotalPrice { 
+            get 
+            {
+                decimal price = 0;
+                Items.ForEach(item => 
+                {
+                    price += (item.Price * item.Quantity);
+                });
+                return price;
+            } 
+        }
+    }
+}
---
+++ Microservices/Smartshop.Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Smartshop.Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
